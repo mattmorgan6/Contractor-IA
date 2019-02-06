1b59a2f6a947ec4dafef68b66da9aa5670d7ce93
mattmorgan6 <mattmorgan6@gmail.com>
2019-02-05 18:40:25 -0800

    Initial commit - solution + data library scaffolding

--- ContractorFind/Controllers/HomeController.cs
+++ ContractorFind/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ContractorFind.Models;
 
 namespace ContractorFind.Controllers
 {
@@ -19,6 +20,35 @@ namespace ContractorFind.Controllers
 
             return View();
         }
+
+
+        public ActionResult CreateGig()
+        {
+            ViewBag.Message = "Create a gig";
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateGig(Gig gig)
+        {
+            ViewBag.Message = "Creat a gig";
+
+            if (ModelState.IsValid)
+            {
+                string t = gig.Title;
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+
+
+
+
 
         public ActionResult Contact()
         {
---
+++ ContractorFind/Models/Gig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContractorFind.Models
+{
+    public class Gig
+    {
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public int Footprint { get; set; }    //in squarefeet
+        public string Description { get; set; }
+        public int Zipcode { get; set; }
+    
+        //price needed in the future possibly.
+    }
+}
