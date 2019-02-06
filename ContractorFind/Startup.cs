3e44878933c68592a53dbf4c68c07be44052294b
mattmorgan6 <mattmorgan6@gmail.com>
2019-02-05 16:07:47 -0800

    Add project files.

---
+++ ContractorFind/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContractorFind.Startup))]
+namespace ContractorFind
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
